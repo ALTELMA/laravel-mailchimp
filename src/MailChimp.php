709172e33698a93c901f4bade3a657e24efd0f17
Phongthorn Kumkankaew <nrohtgnohp@gmail.com>
2015-09-16 16:55:37 +0700

    Update MailChimp.php

--- src/MailChimp.php
+++ src/MailChimp.php
@@ -18,26 +18,68 @@ class MailChimp
 		}
 	}
 
-	private function getOptions()
+	public function get($method, $args = array())
 	{
-
+		return $this->makeRequest('get', $method, $args);
 	}
 
-	public function makeRequest($request, $method)
+	public function post($method, $args = array())
 	{
-		$url = $this->endpoint . 'lists/57ff8a8389';
-		echo $url;
+		return $this->makeRequest('post', $method, $args);
+	}
+
+	public function patch($method, $args = array())
+	{
+		return $this->makeRequest('patch', $method, $args);
+	}
+
+	public function put($method, $args = array())
+	{
+		return $this->makeRequest('put', $method, $args);
+	}
+
+	public function delete($method, $args = array())
+	{
+		return $this->makeRequest('delete', $method, $args);
+	}
+
+	private function makeRequest($request, $method, $args = array())
+	{
+		$url = $this->endpoint . $method;
+		$json_data = json_encode($args, JSON_FORCE_OBJECT);
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/vnd.api+json',
-			'Content-Type: application/vnd.api+json'));
+			'Content-Type: application/vnd.api+json',
+			'Authorization: apikey ' . $this->apikey));
 		curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+
+		switch($request) {
+			case 'post':
+				curl_setopt($ch, CURLOPT_POST, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+			break;
+
+			case 'get':
+				$query = http_build_query($args);
+				curl_setopt($ch, CURLOPT_URL, $url.'?'.$query);
+			break;
+
+			case 'delete':
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+			break;
+
+			case 'patch':
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+			break;
+		}
 
 		$result = curl_exec($ch);
 		curl_close($ch);
