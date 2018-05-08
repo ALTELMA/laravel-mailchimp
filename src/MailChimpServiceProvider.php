75f6905d90008cb76282d82cf457a2ce5756641e
Phongthorn Khamkankaew <nrohtgnohp@gmail.com>
2018-05-08 03:53:28 +0000

    Apply fixes from StyleCI

--- src/MailChimpServiceProvider.php
+++ src/MailChimpServiceProvider.php
@@ -4,7 +4,7 @@ namespace Altelma\LaravelMailChimp;
 
 use Illuminate\Support\ServiceProvider;
 
-class MailchimpServiceProvider extends ServiceProvider
+class MailChimpServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
@@ -12,7 +12,7 @@ class MailchimpServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {        
+    {
         $this->publishes([
             __DIR__.'/../config/mailchimp.php' => config_path('mailchimp.php'),
         ], 'mailchimp');
@@ -24,15 +24,16 @@ class MailchimpServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {        
+    {
         $this->mergeConfigFrom(__DIR__.'/../config/mailchimp.php', 'mailchimp');
         $this->app->singleton('MailChimp', function ($app) {
             $config = $app->make('config');
             $token = $config->get('mailchimp.apikey');
+
             return new MailChimpService($token);
         });
     }
-    
+
     public function provides()
     {
         return ['MailChimp'];
